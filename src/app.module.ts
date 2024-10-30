a6edb6a0e138c7998b93d9c10a6886c7beb1f0b0
Lílian Maria <lfonseca_damico@hotmail.com>
2024-10-30 15:34:01 -0300

    url

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,21 +12,17 @@ import { UsersModule } from './app/users/users.module';
       useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
         const dbConfig: TypeOrmModuleOptions = {
           type: 'postgres',
-          host: configService.get<string>('DB_HOST', 'localhost'),
-          port: configService.get<number>('DB_PORT', 5432),
-          username: configService.get<string>('DB_USERNAME', 'postgres'),
-          password: configService.get<string>('DB_PASSWORD', 'your-password'),
-          database: configService.get<string>('DB_DATABASE', 'mindcare'),
+          url: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
           entities: [__dirname + '/**/*.entity.{js,ts}'],
           synchronize: true,
           logging: true,
         };
-      
-        console.log('Database Config:', dbConfig);
-      
+
         return dbConfig;
       },
-      
     }),
     UsersModule,
   ],
