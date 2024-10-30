422b88fed582bb48aeb7c45a8d54e6050c41c761
Lílian Maria <lfonseca_damico@hotmail.com>
2024-10-30 15:14:53 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  
   dotenv.config();
   console.log(process.env.DB_HOST, process.env.DB_USERNAME, process.env.DB_PASSWORD);
-
-
+  
   const app = await NestFactory.create(AppModule);
-
+  
+  app.use(cors()); 
   
   const config = new DocumentBuilder()
     .setTitle('MindCareApp API')
     .setDescription('API para gerenciamento do MindCare')
     .setVersion('1.0')
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
   
   app.useGlobalPipes(new ValidationPipe());
 
-  
   await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
+
