351d0674c3fa49338dffca25a2cafb914a89c83b
Lilian Maria Damico da Fonseca <100726998+LilianDamico@users.noreply.github.com>
2024-10-31 21:08:22 -0300

    Update app.module.ts
    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,26 +6,29 @@ import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(), // Carrega variáveis de ambiente do .env
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
         const dbConfig: TypeOrmModuleOptions = {
           type: 'postgres',
-          ssl: {
-            rejectUnauthorized: false,
-          },
-          entities: [__dirname + '/**/*.entity.{js,ts}'],
-          synchronize: true,
-          logging: true,
+          host: configService.get<string>('DATABASE_HOST'), // Obtém o host do banco de dados
+          port: parseInt(configService.get<string>('DATABASE_PORT'), 10), // Obtém a porta do banco de dados
+          username: configService.get<string>('DATABASE_USER'), // Obtém o usuário do banco de dados
+          password: configService.get<string>('DATABASE_PASS'), // Obtém a senha do banco de dados
+          database: configService.get<string>('DATABASE_NAME'), // Obtém o nome do banco de dados
+          entities: [__dirname + '/**/*.entity.{js,ts}'], // Localização das entidades
+          synchronize: true, 
+          logging: true, // Habilita logs para debug
         };
 
         return dbConfig;
       },
     }),
-    UsersModule,
+    UsersModule, // Seu módulo de usuários
   ],
-  controllers:[AppController]
+  controllers: [AppController], // Controllers da aplicação
 })
 export class AppModule {}
+
